0db6380fef8009d6af2fdd666d7e8a8c0b4a57a2
DamonCais <516181085@qq.com>
2018-12-17 11:26:38 +0800

    base commit

---
+++ src/pages/list.js
@@ -0,0 +1,52 @@
+export default [
+    [
+        {
+            type: 'swiper',
+            photoList: [
+                {
+                    photo:
+                        'https://j-image.missfresh.cn/img_20181114145542681.jpg?iopcmd=thumbnail&type=4&width=640'
+                },
+                { photo: '../images/icon_alert.png' }
+            ]
+        },
+        { type: 'cell', title: '全国送今日热卖' },
+        {
+            type: 'card',
+            title: '刀唛一级压榨花生油5L',
+            desc: '只取一级压榨精华',
+            price: '19.9',
+            imageURL:
+                'http://missfresh-fms.ufile.ucloud.cn/525ec890234c44cfa147192c87bdcf06.jpg?iopcmd=thumbnail&type=4&width=200'
+        },
+        { type: 'cell', title: '订阅到货提醒', label: '货到了 我会记得喊声你哦' },
+        {
+            type: 'card',
+            title: '刀唛一级压榨花生油5L',
+            desc: '只取一级压榨精华',
+            price: '19.9',
+            imageURL:
+                'http://missfresh-fms.ufile.ucloud.cn/525ec890234c44cfa147192c87bdcf06.jpg?iopcmd=thumbnail&type=4&width=200'
+        }
+    ],
+    [
+        { type: 'cell', title: '全国送今日热卖111' },
+        {
+            type: 'card',
+            title: '刀唛一级压榨花生油5L111',
+            desc: '只取一级压榨精华111',
+            price: '29.9',
+            imageURL:
+                'http://missfresh-fms.ufile.ucloud.cn/525ec890234c44cfa147192c87bdcf06.jpg?iopcmd=thumbnail&type=4&width=200'
+        },
+        { type: 'cell', title: '订阅到货提醒', label: '货到了 我会记得喊声你哦' },
+        {
+            type: 'card',
+            title: '刀唛一级压榨花生油5L',
+            desc: '只取一级压榨精华',
+            price: '19.9',
+            imageURL:
+                'http://missfresh-fms.ufile.ucloud.cn/525ec890234c44cfa147192c87bdcf06.jpg?iopcmd=thumbnail&type=4&width=200'
+        }
+    ]
+]
